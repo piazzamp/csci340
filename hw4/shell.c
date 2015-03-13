40d0dbdbb9bb5c89aeffea76cdb85c327861c59a
Matt Piazza <piazzamp@g.cofc.edu>
2015-03-13 18:39:27 -0400

    find & destroy memory leak

--- hw4/shell.c
+++ hw4/shell.c
@@ -203,8 +203,8 @@ int execute( command_t* p_cmd ){
             return -1;
 			}
 		}
-		return waitpid(cpid, &child_status, 0);
 		p_cmd->argv[redirindex] = redir_p;
+		return waitpid(cpid, &child_status, 0);	
 	}
 	return -1;
 /*
