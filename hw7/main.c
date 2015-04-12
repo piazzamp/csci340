629936b30eb527cffd302cff01bd44a9ea5fe1ad
Matt Piazza <piazzamp@g.cofc.edu>
2015-04-11 20:32:14 -0400

    add 2/3 of hw7

---
+++ hw7/main.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "mem.h"
+
+#define DEBUG 1
+
+/* minimum and maximum duration of use for an allocated block of memory */
+#define MIN_DURATION      3
+#define MAX_DURATION     25
+
+/* minimum and maximum allocation request size */
+#define MIN_REQUEST_SIZE    3
+#define MAX_REQUEST_SIZE  100
+
+/*
+  The main program will accept four paramemters on the command line.
+  The first parameter is the memory size.  The second parameter is the
+  number of times to repeat each experiment (ie. number of runs).  The
+  third parameter is the duration of the each simulation run. The
+  forth parameter is a random number seed. Here is an example command
+  line:
+
+  ./hw7 10000 100 2000 1234
+
+  This means that your program should initialize physical memory to
+  10,000 units, perform 100 runs with each run taking 2000 units of
+  time, and the random number generator should be seeded (one time)
+  with the value 1234.
+*/
+
+int main(int argc, char** argv){
+	int memsize, runs, iterations, seed, method, i, j, sizerange, durationrange, dur, siz, total_frags = 0, total_misses = 0, total_probes = 0, result;
+	// double durationscale, sizescale;
+	enum mem_strategies strategy;
+	char *strat_string;
+	if (argc != 5){
+		fprintf(stderr, "incorrect number of args. I want 4, you gave me %d\nusage: ./%s <memsize> <number of runs> <duration> <seed>\n", argc, argv[0]);
+	exit(1);
+	}
+	memsize = atoi(argv[1]);
+	runs = atoi(argv[2]);
+	iterations = atoi(argv[3]);
+	seed = atoi(argv[4]);
+	mem_init(memsize);
+
+	srand(seed);
+	// then mult the percentages by the appropriate scale:
+	durationrange = MAX_DURATION - MIN_DURATION;
+	sizerange = MAX_REQUEST_SIZE - MIN_REQUEST_SIZE;
+	for (method = 0; method <= 2; method++){
+		total_frags = 0;
+		total_misses = 0;
+		total_probes = 0;
+		switch (method){
+			case 0:
+				strategy = FIRST;
+				strat_string = "FIRST\0";
+				break;
+			case 1:
+				strategy = NEXT;
+				strat_string = "NEXT\0";
+				break;
+			case 2:
+				strategy = BEST;
+				strat_string = "BEST\0";
+				break;
+			default:
+				fprintf(stderr, "oh golly, I broke the enum iterator\n");
+				exit(1);
+		}
+		for (i = 0; i < runs; i++){
+			// each iteration in this loop is a full test with a single
+			// placement strategy
+
+			for (j = 0; j < iterations; j++){
+				// each iteration in this loop represents one time unit
+
+				// use one set of memory for all of *iterations*
+				// then clear it out and do it again (outer loop)
+
+				// get random number in the right range
+				// by getting a num between 0-range, then adding min
+				dur = (rand() % (durationrange + 1)) + MIN_DURATION;
+   			siz = (rand() % (sizerange + 1)) + MIN_REQUEST_SIZE;
+				//if (DEBUG) { printf("using duration %d and size %d\n", dur, siz); }	
+				result = mem_allocate(strategy, siz, dur);
+				if (result == -1){
+					total_misses += 1;
+				}
+				else {
+					if (DEBUG) { printf("incremented total_probes(%d) by %d\n", total_probes, result); }
+					total_probes += result;
+				}
+				mem_single_time_unit_transpired();
+			}
+			total_frags += mem_fragment_count(3);
+		}
+	printf("%s:\tavg fragmentation: %.3f\tavg failures: %.3f\tavg probes: %.5f\n", strat_string, ((double) total_frags) / ((double) runs), ((double) total_misses)/((double) runs), ((double) total_probes)/((double) runs));
+	//printf("average number of fragments for method %s: %.3f\n", strat_string, ((double) total_frags) / ((double) runs));
+	}
+
+  return 0;
+}
---
+++ hw7/mem.c
@@ -0,0 +1,238 @@
+#include <stdlib.h>   // for malloc() and free()
+#include <stdio.h>
+
+#include "mem.h"
+
+#define DEBUG 1
+
+/*
+  Physical memory array. This is a static global array for all functions in this file.  
+  An element in the array with a value of zero represents a free unit of memory.
+*/
+static unsigned int* memory;
+
+/*
+ The size (i.e. number of units) of the physical memory array. This is a static global 
+ variable used by functions in this file.  
+
+ */
+static unsigned int mem_size;
+
+
+/*
+ The last_placement_position variable contains the end position of the last 
+ allocated unit used by the next fit placement algorithm.  
+ */
+static unsigned int last_placement_position;
+
+void print_mem(){
+	int i;
+	printf("memory: ");
+	for (i = 0; i < mem_size; i++){
+		printf("%d ", memory[i]);
+	}
+	printf("\n");
+}
+
+int getchunk(int startindex){
+	int i, cursize = 0;
+	for (i = startindex; i < mem_size; i++){
+		if (memory[i] == 0){
+			cursize++;
+		}
+		else break;
+	}
+	if (DEBUG) { printf("[getchunk(%d)] found chunk with length %d\n", startindex, cursize); }
+	return cursize;
+}
+
+int getfirstempty(){
+	int i;
+	for (i = 0; i < mem_size; i++){
+		if (memory[i] == 0){
+			if (DEBUG) { printf("[getfirstempty] %d\n", i); }
+			return i;
+		}
+	}
+	return -1;
+}
+
+void allocate(int size, int duration, int startindex){
+	int x;
+	for (x = 0; x < size; x++){
+		memory[startindex + x] = duration;
+	}
+	if (DEBUG) { printf("allocated %d blocks at index %d\n", size, startindex); }
+}
+
+int firstfit(int size, int duration){
+	int i, chunksize, done = 0, tries = 0;
+	i = getfirstempty();
+	if (i == -1){
+		// no free slots, return -1 for failure to allocate
+		return -1;
+	}
+	while (i < mem_size && !done){
+		if (DEBUG) { printf("[firstfit] i < mem_size:: %d < %d\n", i, mem_size); }
+		if (DEBUG) { printf("TRIES %d\ni: %d\n", tries, i); }
+		chunksize = getchunk(i);
+		if (chunksize <= 0){
+			i++;
+			//return -1;
+		}
+		else if (chunksize >= size){
+			// allocate it
+			allocate(size, duration, i);
+			done = 1;
+			if (DEBUG) { printf("[firstfit] allocated successfully\n"); }
+		}
+		else {
+			if (DEBUG) { printf("[firstfit] probed and missed: %d\n", tries); }
+			tries += 1;
+			i += chunksize;
+		}
+	}
+	if (DEBUG) { print_mem(); }
+	if (DEBUG) { printf("[firstfit] done: %d\n", done);  }
+	if (done) {
+		if (DEBUG) { printf("[firstfit] probed %d chunks\n", tries); }
+		return tries;
+	}
+	else{	
+		return -1;
+	}
+}
+
+int nextfit(int size, int duration){
+	int i, chunksize, done = 0, tries = 0;
+	i = last_placement_position;
+	while (i < mem_size && !done){
+		chunksize = getchunk(i);
+		if (chunksize <= 0){
+			if (DEBUG) { printf("[nextfit] looking at a 0-sized chunk, i++\n"); }
+			i++;
+		}
+		else if (chunksize >= size){
+			if (DEBUG) { printf("[nextfit] allocated successfully\n"); }
+			allocate(size, duration, i);
+			done = 1;
+		}
+		else {
+			// just looked at a chunk of 0's that was too small
+			// aka: probed a block of memory 
+			tries++;
+			i += chunksize;
+		}
+	}
+	if (done){
+		return tries;
+	} else {
+		return -1;
+	}
+}
+
+int bestfit(int size, int duration){
+	// scoop
+	return 0;
+}
+/*
+  Using the memory placement algorithm, strategy, allocate size
+  units of memory that will reside in memory for duration time units.
+
+  If successful, this function returns the number of contiguous blocks
+  (a block is a contiguous "chuck" of units) of free memory probed while 
+  searching for a suitable block of memory according to the placement 
+  strategy specified.  If unsuccessful, return -1.
+
+  If a suitable contiguous block of memory is found, the first size
+  units of this block must be set to the value, duration.
+ */
+int mem_allocate(mem_strategy_t strategy, unsigned int size, unsigned int duration){
+	int result;
+
+	switch (strategy){
+		case FIRST:
+			if (DEBUG) { printf("using FIRST method\n"); }
+			result = firstfit(size, duration);
+			break;
+		case NEXT:
+			result = nextfit(size, duration);
+			break;
+		case BEST:
+			result = bestfit(size, duration);
+			break;
+		default:
+			fprintf(stderr, "please use a strategy in set {FIRST, NEXT, BEST}\n");
+			exit(1);
+	}
+	return result;
+}
+
+/*
+  Go through all of memory and decrement all positive-valued entries.
+  This simulates one unit of time having transpired.
+ */
+int mem_single_time_unit_transpired(){
+	int i;
+	for (i = 0; i < mem_size; i++){
+		if (memory[i] > 0){
+			memory[i]--;
+		}
+	}
+	return 0; // ???
+}
+
+/*
+  Return the number of fragments in memory.  A fragment is a
+  contiguous free block of memory of size less than or equal to
+  frag_size.
+ */
+int mem_fragment_count(int frag_size){
+	int cursize = 0, i, count = 0;
+	for (i = 0; i < mem_size; i++){	
+		if (memory[i] == 0){
+			// still in a chunk
+			cursize++;
+		}
+		else{
+			if (DEBUG) { printf("[frag counter] found a chunk of  size %d\n", cursize); }
+			if (cursize <= frag_size && cursize > 0){
+				count++;
+			}
+			cursize = 0;
+		}
+	}
+	if (DEBUG) { printf("[frag_count] returning %d\n", count);  }
+	return count;
+}
+
+/*
+  Set the value of zero to all entries of memory.
+ */
+void mem_clear(){
+	int i;
+	for (i = 0; i < mem_size; i++){
+		memory[i] = 0;
+	}
+	last_placement_position = 0;
+}
+
+/*
+ Allocate physical memory to size. This function should 
+ only be called once near the beginning of your main function.
+ */
+void mem_init( unsigned int size )
+{
+	memory = malloc( sizeof(unsigned int)*size );
+	mem_size = size;
+	last_placement_position = 0;
+	mem_clear();
+}
+
+/*
+ Deallocate physical memory. This function should 
+ only be called once near the end of your main function.
+ */
+void mem_free(){
+	free( memory );
+}
