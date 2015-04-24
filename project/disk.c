4d001575e7fd37bcd0dd1f8279c976d9c251b832
Matt Piazza <piazzamp@g.cofc.edu>
2015-04-24 18:45:47 -0400

    pretty much done and rm useless utils

--- project/disk.c
+++ project/disk.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 
 #include "disk.h"
-#include "utils.h"
+
+#define DEBUG 1
 
 // initializes device and returns a 
 // disk_t full of metadata about the floppy
@@ -12,9 +13,8 @@
 // geometry_t: bytesPerSector, sectorsPerTrack, heads, cylinders (all uints)
 Disk physical_disk(char* name){
 	disk_t *disk = (disk_t*) malloc(sizeof(disk_t));
-	geometry_t *geo = (geometry_t*) malloc(sizeof(geometry_t));
-	handle_t fd = open(name, O_RDONLY);
-	char buf[100];
+	// geometry_t *geo = (geometry_t*) malloc(sizeof(geometry_t));
+	handle_t fd = open(name, O_RDONLY);	
 	unsigned int tmp = 0;
 	int num_sectors;	
 
@@ -22,17 +22,7 @@ Disk physical_disk(char* name){
 		fprintf(stderr, "failed to open %s, are you sure it exists?\n", name);
 		exit(1);
 	}
-/*	if (lseek(fd, 3, SEEK_SET) < 0){
-		fprintf(stderr, "failed to seek\n");
-		exit(1);
-	}
-	if (read(fd, buf, 7) < 0){
-		fprintf(stderr, "failed to read file\n");
-		exit(1);
-	}
-	buf[7] = '\0';
-	printf("[3-10] floppy manufacturer bytes: %s\n", buf);
-*/
+
 	if (lseek(fd, 11, SEEK_SET) < 0){
 		fprintf(stderr, "failed to seek (11)\n");
 		exit(1);
@@ -41,8 +31,9 @@ Disk physical_disk(char* name){
 		fprintf(stderr, "failed to read number of bytes per sector\n");
 		exit(1);
 	}
-	geo->bytesPerSector = tmp;
-	printf("[11-12] bytes per sector: %d\n", geo->bytesPerSector);
+	//geo->bytesPerSector = tmp;
+	disk->geometry.bytesPerSector = tmp;
+	if (DEBUG) { printf("[11-12] bytes per sector: %d\n", disk->geometry.bytesPerSector); }
 
 	if (lseek(fd, 19, SEEK_SET) <  0){
 		fprintf(stderr, "failed to seek (19)\n");
@@ -53,7 +44,7 @@ Disk physical_disk(char* name){
 		exit(1);
 	}	
 	num_sectors = tmp;
-	printf("[19-20] num of sectors: %d\n",num_sectors);
+	if (DEBUG) { printf("[19-20] num of sectors: %d\n",num_sectors); }
 
 	if (lseek(fd, 26, SEEK_SET) < 0){
 		fprintf(stderr, "failed to seek to 26\n");
@@ -63,8 +54,8 @@ Disk physical_disk(char* name){
 		fprintf(stderr, "failed to read heads\n");
 		exit(1);
 	}	
-	geo->heads = tmp;
-	printf("[26-27] heads %d\n", geo->heads);
+	disk->geometry.heads = tmp;
+	if (DEBUG) { printf("[26-27] heads %d\n", disk->geometry.heads); }
 
 	if (lseek(fd, 24, SEEK_SET) < 0){
 		fprintf(stderr, "failed to seek to 24\n");
@@ -74,22 +65,68 @@ Disk physical_disk(char* name){
 		fprintf(stderr, "failed to read sectors per track\n");
 		exit(1);
 	}
-	geo->sectorsPerTrack = tmp;
-	printf("[24-25] sectors per track: %d\n", geo->sectorsPerTrack);
+	disk->geometry.sectorsPerTrack = tmp;
+	if (DEBUG) { printf("[24-25] sectors per track: %d\n", disk->geometry.sectorsPerTrack); }
 
-	geo->cylinders = num_sectors / geo->sectorsPerTrack;
-	printf("[     ] cylinders: %d\n", geo->cylinders);
-	disk->floppyDisk = fd;
-	disk->geometry = *geo;
+	disk->geometry.cylinders = num_sectors / disk->geometry.sectorsPerTrack;
+	if (DEBUG) { printf("[     ] cylinders: %d\n", disk->geometry.cylinders); }
+
+	disk->floppyDisk = fd;	
 	return disk;
+	// when close(fd)??????
 }
 
 // reads data from the given logical sector and enters it into the buffer
 // allocate the buffer within this function, make sure it's big enough, etc
-int sector_read(Disk theDisk, unsigned int logicalSectorNumber, unsigned char* buffer);
+int sector_read(Disk theDisk, unsigned int logicalSectorNumber, unsigned char* buffer){
+	unsigned int offset = logicalSectorNumber * theDisk->geometry.bytesPerSector;
+	if (DEBUG) { printf("seeking to %d\nfloppydisk: %d\n", offset, theDisk->floppyDisk); }
+	if (lseek(theDisk->floppyDisk,offset, SEEK_SET) < 0){
+		fprintf(stderr, "failed to seek to %d\n", offset);
+		return 0;
+	}
+	if (read(theDisk->floppyDisk, buffer, theDisk->geometry.bytesPerSector) < 0){
+		fprintf(stderr, "failed to read at %d\n", offset);
+		return 0;
+	}
+	return 1;
+}
 
 // calls sector_read, then prints it using one of the following types:
 // 'x': hex
 // 'c': char
 // 'o': octal 
-void sector_dump(Disk theDisk, int logicalSectorNumber, char type);
+void sector_dump(Disk theDisk, int logicalSectorNumber, char type){
+	unsigned char *buf = (unsigned char*) malloc(theDisk->geometry.bytesPerSector);
+	char *str;
+	int i, j, offset;
+
+	if(sector_read(theDisk, logicalSectorNumber, buf) == 0){
+		fprintf(stderr, "failed to read sector %d\n", logicalSectorNumber);
+		exit(1);
+	}
+	offset = logicalSectorNumber * theDisk->geometry.bytesPerSector;
+
+	switch(type){
+		case 'x':
+			str = "%01x\t";
+			break;
+		case 'c':
+			str = "%c\t";
+			break;
+		case 'o':
+			str = "%o\t";
+			break;
+		default:
+			fprintf(stderr, "please use a type in the set {c, o, x}\n");
+			exit(1);
+	}
+	for (i = 0; i < 32; i++){
+		printf("0x%08x\t", offset + (i * 16));
+		for (j = 0; j < 16; j++){
+			printf(str, buf[(i * 16) + j]);
+		}
+		printf("\n");
+	}
+	free(buf);
+}
--- project/main.c
+++ project/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "disk.h"
 
+
+// usage: main floppy.img sectornum {c,x,o}
 int main(int argc, char **argv){
-	physical_disk("floppy.img");
+	Disk disk;
+	int sector;
+	char type;
+
+	if (argc < 4){
+		fprintf(stderr, "usage: %s <fat16 image file> <sector num> {c,x,o}\nwhere c, x, or o indicate character, hex, or octal print formatting\n\n", argv[0]);
+		exit(1);
+	}
+	type = argv[3][0];
+	sector = atoi(argv[2]);
+	printf("got sector num: %d\n", sector);
+	disk = physical_disk(argv[1]);
+		
+	sector_dump(disk, sector, type);
+
 	return 0;
 }
